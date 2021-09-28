9c31101bb8fb43467a6c796642e6916a64b83472
t-nagasaka <66255777+t-nagasaka@users.noreply.github.com>
2021-09-29 07:42:59 +0900

    done create_TemplateView

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic.base import (View,)
+from django.views.generic.base import (View, TemplateView)
 from . import forms
+from datetime import datetime
 
 
 class IndexView(View):
@@ -17,3 +18,14 @@ class IndexView(View):
         return render(request, 'index.html', context={
             'book_form': book_form
         })
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(kwargs)
+        context["name"] = kwargs.get('name')
+        context["time"] = datetime.now()
+        return context
