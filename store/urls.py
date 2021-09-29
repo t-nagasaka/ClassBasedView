77bd15f0a1b668da57c8f21461669d25940819aa
t-nagasaka <66255777+t-nagasaka@users.noreply.github.com>
2021-09-29 23:13:05 +0900

    done create_DetailView

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import IndexView, HomeView, BookDetailView
+from .views import IndexView, HomeView, BookDetailView, BookListView
 # from django.views.generic.base import TemplateView
 
 
@@ -10,4 +10,6 @@ urlpatterns = [
     # path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
     path('home/<name>', HomeView.as_view(), name='home'),
     path('detail_book/<int:pk>', BookDetailView.as_view(), name='detail_book'),
+    path('list_books/', BookListView.as_view(), name='list_books'),
+    path('list_books/<name>', BookListView.as_view(), name='list_books'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import (View, TemplateView)
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from . import forms
 from datetime import datetime
 from .models import Books
@@ -37,9 +38,20 @@ class HomeView(TemplateView):
 class BookDetailView(DetailView):
     model = Books
     template_name = 'book.html'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         pprint.pprint(vars(context['books']))
         return context
-    
+
+
+class BookListView(ListView):
+    model = Books
+    template_name = 'book_list.html'
+
+    def get_queryset(self):
+        qs = super(BookListView, self).get_queryset()
+        if 'name' in self.kwargs:
+            qs = qs.filter(name__startswith=self.kwargs['name'])
+        qs = qs.order_by('description')
+        return qs
